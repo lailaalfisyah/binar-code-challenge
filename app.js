612021765ea3e9ad40985299723eaf61442c0582
Laila Alfi Syah <lailaalfisyah@gmail.com>
2021-11-01 22:30:02 +0700

    Chapter 5

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// menggunakan view engine ejs
+app.set('view engine', 'ejs');
+
+// middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+app.use(logger);
+app.use(express.json());
+app.use(express.static('public'));
+
+// route
+const router = require('./router');
+app.use(router);
+
+// nyalakan web server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
---
+++ router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+let users = require('./data/users.json');
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Landing Page'
+    })
+})
+
+router.get('/login', (req, res) => {
+    res.status(200).json(users);
+})
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const name = users[0].name;
+    // const user = { name, username, password };
+    const matchUser = users.find(data => data.username === username);
+    const matchPass = users.find(data => data.password === password);
+
+    if (!matchUser && !matchPass) {
+        res.send('Hmm ... username dan password kamu salah nih')
+    } else if (matchUser) {
+        if (matchPass) {
+            // users.push(user);
+            console.log(`Hai, ${name}! Kamu telah berhasil login.`)
+            res.redirect('/suit-game')
+        } else {
+            res.send(`Ups! Salah password, silakan coba lagi ya.`);
+            
+        }
+    } else {
+        res.send('Ups! Username yang kamu masukkan salah.');
+    }  
+})
+
+router.get('/suit-game', (req, res) => {
+    res.render('suit-game', {
+        title: 'Suit'
+    })
+})
+
+module.exports = router;
